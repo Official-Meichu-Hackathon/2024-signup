9e115f958863bcfe400694604ed1febc7731807c
Sean20405 <Sean20405>
2024-07-27 17:15:48 +0800

    add: navbar and footer

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="fixed top-0 w-full h-[75px] bg-white">
+    <div class="flex items-center justify-between mx-11 h-full">
+      <div class="flex items-center justify-between gap-x-12 mt-1.5">
+        <router-link to="/" class="w-[15vw] font-Mantou font-normal text-2xl text-[#43473E]">梅竹黑克松</router-link>
+        <div class="text-base font-Inter font-extrabold text-yellow">首頁</div>
+        <div class="text-base font-Inter font-medium text-[#222236]">比賽時程 testt</div>
+        <div class="text-base font-Inter font-medium text-[#222236]">題目說明 testt</div>
+        <div class="text-base font-Inter font-medium text-[#222236]">參賽數據</div>
+      </div>
+      <div class="flex w-28 h-11 mr-8 mt-2 justify-center items-center gap-2.5 rounded-lg bg-yellow">
+        <p class="font-Inter font-bold text-white">我要報名</p>
+      </div>
+    </div>
+    <hr class="mx-8 bg-[#0F112B] h-[1px]">
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+import colors from "tailwindcss/colors";
+
+module.exports = {
+  content: [
+    './index.html',
+    './src/*.{vue,js,ts,jsx,tsx}',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './src/**/**/*.{vue,js,ts,jsx,tsx}'
+  ],
+  theme: {
+    screens: {
+			xs: "360px",
+			sm: "576px",
+			md: "768px",
+			lg: "992px",
+			xl: "1200px",
+			"2xl": "1400px",
+			"3xl": "1800px",
+		},
+    extend: {
+      colors: {
+        ...colors,
+        "plum": "#644646",
+        "yellow": "#D8E022",
+        "dark-gray": "#43473E",
+        "gray": "#A1A8AD",
+      },
+      fontFamily: {
+        "Mantou": ["Mantou Sans", "serif"],
+        "Poppins": ["Poppins", "serif"],
+        "Inter": ["Inter", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
