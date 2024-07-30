8d10691af22d05311b4dc899fbcc2de1f0ec8688
Sean20405 <Sean20405>
2024-07-30 12:59:15 +0800

    fix (navbar): z-index and logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,18 @@
 <template>
-  <nav class="fixed top-0 w-full h-[75px] bg-white">
-    <div class="flex items-center justify-between mx-11 h-full">
-      <div class="flex items-center justify-between gap-x-12 mt-1.5">
-        <router-link to="/" class="w-[15vw] font-Mantou font-normal text-2xl text-[#43473E]">梅竹黑克松</router-link>
+  <nav class="fixed top-0 w-full h-[75px] z-50 bg-white">
+    <div class="flex items-center justify-between mx-[3vw] h-full">
+      <div class="flex items-center justify-between gap-x-[3vw] mt-1.5">
+        <router-link to="/"><img src="@/../public/Header_Logo.svg"/></router-link>
         <div class="text-base font-Inter font-extrabold text-yellow">首頁</div>
-        <div class="text-base font-Inter font-medium text-[#222236]">比賽時程 testt</div>
-        <div class="text-base font-Inter font-medium text-[#222236]">題目說明 testt</div>
+        <div class="text-base font-Inter font-medium text-[#222236]">比賽時程</div>
+        <div class="text-base font-Inter font-medium text-[#222236]">題目說明</div>
         <div class="text-base font-Inter font-medium text-[#222236]">參賽數據</div>
       </div>
-      <div class="flex w-28 h-11 mr-8 mt-2 justify-center items-center gap-2.5 rounded-lg bg-yellow">
+      <div class="flex w-[11vw] lg:w-[7.6vw] h-11 mr-8 mt-2 justify-center items-center gap-2.5 rounded-lg bg-yellow">
         <p class="font-Inter font-bold text-white">我要報名</p>
       </div>
     </div>
-    <hr class="mx-8 bg-[#0F112B] h-[1px]">
+    <hr class="mx-[2vw] bg-[#0F112B] h-[1px]">
   </nav>
 </template>
 
