9006806ee1c7f1b6e55ff09ec57d8478d96c70dc
chencxxx <judy940825@gmail.com>
2024-08-01 16:11:21 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <Navbar/>
   <router-view/>
+  <TopicDesc/>
   <Footer/>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue'
 import Footer from '@/components/Footer.vue'
-
+import TopicDesc from './components/TopicDesc.vue';
 export default {
   components: {
     Navbar,
-    Footer
+    Footer,
+    TopicDesc
   }
 }
 </script>
---
+++ src/components/TopicDesc.vue
@@ -0,0 +1,17 @@
+<template>
+    <CompanyFrame/>
+</template>
+    
+<script>
+import CompanyFrame from './CompanyFrame.vue';
+export default {
+    name: 'TopicDesc',
+    components: {
+        CompanyFrame
+    }
+}
+</script>
+    
+<style>
+    
+</style>
