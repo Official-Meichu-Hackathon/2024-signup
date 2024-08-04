1859d8eb020a9a64153b559703348cc78fd5acfd
ChenCXxx <judy940825@gmail.com>
2024-08-05 04:07:19 +0800

    rename(Top): rename top image

--- src/components/Company/Top.vue
+++ src/components/Company/Top.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="hidden md:flex lg:max-h-[100px] w-full h-[15vh] justify-between items-center mt-[8%]">
-        <object data="Company\Group 43.svg"
+        <object data="Company\top_left.svg"
         type="image/svg+xml"
         class="flex max-w-[250px] w-[20vw] aspect-w-249 aspect-h-78 flex-shrink-0"></object>
         <div class="flex w-full h-full pl-[2vw] pr-[2vw] justify-between items-center">
             <object class="relative border-dark-gray flex-grow h-[45%] mx-auto flex-shrink-0 rounded-[25px] border-[3px] border-dashed "></object>
         </div>
-        <object data="Company\Group 44.svg"
+        <object data="Company\top_right.svg"
         type="image/svg+xml"
         class="flex max-w-[250px] w-[20vw] aspect-w-249 aspect-h-78 flex-shrink-0"></object>
     </div>
