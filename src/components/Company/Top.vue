bc34af960c0cced407adfe035e2a15c162e80814
ChenCXxx <judy940825@gmail.com>
2024-08-14 02:37:21 +0800

    fix: top object size

--- src/components/Company/Top.vue
+++ src/components/Company/Top.vue
@@ -3,8 +3,8 @@
         <object data="Company\top_left.svg"
         type="image/svg+xml"
         class="flex max-w-[250px] w-[20vw] aspect-w-249 aspect-h-78 flex-shrink-0"></object>
-        <div class="flex w-full h-full pl-[2vw] pr-[2vw] justify-between items-center">
-            <object class="relative border-dark-gray flex-grow h-[45%] mx-auto flex-shrink-0 rounded-[25px] border-[3px] border-dashed "></object>
+        <div class="flex w-full h-full pl-[2vw] pr-[2vw] pt-[.5vw] pb-[.5vw] justify-between items-center">
+            <object class="relative border-dark-gray flex-grow h-[45%] mx-auto flex-shrink-0 rounded-[25px] border-[3px] border-dashed"></object>
         </div>
         <object data="Company\top_right.svg"
         type="image/svg+xml"
