5288f3b7d7195da565e4499b3e28c290147dc469
Sean20405 <Sean20405>
2024-08-14 17:07:37 +0800

    add: RWD of navbar and footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,28 +1,75 @@
 <template>
-  <footer class="pd-15">
-    <hr class="mx-8 bg-[#666] h-[0.5px]">
+  <!-- Desktop ver. -->
+  <footer class="hidden md:block pd-15">
+    <div class="mx-8 h-[1px] bg-[#666]"/>
     <div class="inline-flex pt-0.5 pb-[88px] justify-center w-full">
       <div>
         <div class="flex pt-11 gap-x-8 justify-center">
-          <a href="https://www.facebook.com/HackMeiChu"><img src="../../public/facebook.svg"/></a>
-          <a href="https://www.instagram.com/mc_hackathon/"><img src="../../public/instagram.svg"/></a>
-          <a href="mailto:2024mchackathon@gmail.com"><img src="../../public/email.svg"/></a>
+          <a href="https://www.facebook.com/HackMeiChu"><img src="facebook.svg"/></a>
+          <a href="https://www.instagram.com/mc_hackathon/"><img src="instagram.svg"/></a>
+          <a href="mailto:2024mchackathon@gmail.com"><img src="email.svg"/></a>
         </div>
         <div class="font-Mantou font-normal text-sm pt-6">
           Copyright © 2024 Meichu Hackathon
         </div>
       </div>
-        
     </div>
   </footer>
+
+  <!-- Mobile ver. -->
+  <footer class="md:hidden footer-m fixed bottom-0 w-full h-[78px] z-50 bg-white">
+    <div class="flex items-center justify-around h-full mx-[8vw]">
+      <router-link to="/" class="footer-button-m">
+        <img src="footer/homepage_icon.svg">
+        <div>首頁</div>
+      </router-link>
+      <div class="footer-button-m">
+        <img src="footer/timeflow_icon.svg">
+        <div>比賽時程</div>
+      </div>
+      <div class="signup-button flex flex-col items-center justify-center gap-1 mx-[-10px]">
+        <img src="footer/monster.svg">
+        <div>我要報名</div>
+      </div>
+      
+      <div class="footer-button-m">
+        <img src="footer/topic_icon.svg">
+        <div>題目說明</div>
+      </div>
+      <div class="footer-button-m">
+        <img src="footer/statistics_icon.svg">
+        <div>參賽數據</div>
+      </div>
+    </div>
+  </footer>
+
 </template>
 
 <script>
-export default {
 
-}
 </script>
 
 <style>
-
+.footer-m {
+  color: #A1A8AD;
+  font-size: 10px;
+  font-weight: 700;
+  font-family: Inter;
+  text-align: center;
+  box-shadow: 0px -1px 4px 0px rgba(0, 0, 0, 0.25);
+}
+.signup-button {
+  width: 74px;
+  height: 68px;
+  border-radius: 10px;
+  border: 2px solid var(--d-9-d-9-d-9, #D9D9D9);
+  background: #FFF;
+}
+.footer-button-m {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 2.5rem;
+}
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,33 @@
 <template>
-  <nav class="fixed top-0 w-full h-[75px] z-50 bg-white">
+  <!-- Desktop ver. -->
+  <nav class="hidden md:block h-[4.6875rem]">
     <div class="flex items-center justify-between mx-[3vw] h-full">
       <div class="flex items-center justify-between gap-x-[3vw] mt-1.5">
-        <router-link to="/"><img src="@/../public/Header_Logo.svg"/></router-link>
-        <div class="text-base font-Inter font-extrabold text-yellow">首頁</div>
-        <div class="text-base font-Inter font-medium text-[#222236]">比賽時程</div>
-        <div class="text-base font-Inter font-medium text-[#222236]">題目說明</div>
-        <div class="text-base font-Inter font-medium text-[#222236]">參賽數據</div>
+        <router-link to="/"><img src="Header_Logo.svg"/></router-link>
+        <div class="text-highlight">首頁</div>
+        <div class="text-basic">比賽時程</div>
+        <div class="text-basic">題目說明</div>
+        <div class="text-basic">參賽數據</div>
       </div>
       <div class="flex w-[11vw] lg:w-[7.6vw] h-11 mr-8 mt-2 justify-center items-center gap-2.5 rounded-lg bg-yellow">
         <p class="font-Inter font-bold text-white">我要報名</p>
       </div>
     </div>
-    <hr class="mx-[2vw] bg-[#0F112B] h-[1px]">
+  </nav>
+
+  <!-- Mobile ver. -->
+  <nav class="md:hidden h-14">
+    <div class="flex items-center justify-between px-[4.6vw] h-full">
+      <router-link to="/"><img src="Header_Logo_m.svg"/></router-link>
+      <div class="flex items-center justify-between gap-x-[3vw]">
+        <a href="https://www.facebook.com/HackMeiChu"><img src="facebook.svg" class="w-5 h-5"/></a>
+        <a href="https://www.instagram.com/mc_hackathon/"><img src="instagram.svg" class="w-5 h-5"/></a>
+        <a href="mailto:2024mchackathon@gmail.com"><img src="email.svg" class="w-6 h-6"/></a>
+      </div>
+    </div>
   </nav>
 </template>
+
 
 <script>
 export default {
@@ -23,5 +36,27 @@ export default {
 </script>
 
 <style>
+nav {
+  position: fixed;
+  top: 0;
+  z-index: 50;
+  width: 100%;
+  background: white;
+  box-shadow: 0px 1px 4px 0px #00000040;
+}
 
+.text-highlight {
+  font-size: 1rem;
+  line-height: 1.5rem;
+  font-family: Inter, sans-serif;
+  font-weight: 800;
+  color: #d8e022;
+}
+.text-basic {
+  font-size: 1rem;
+  line-height: 1.5rem;
+  font-family: Inter, sans-serif;
+  font-weight: 500;
+  color: #222236;
+}
 </style>
