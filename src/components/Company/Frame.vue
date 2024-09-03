e15d0f17ac8605d77f002fc88aebffe36d722775
Rae <yylee1026@gmail.com>
2024-09-03 14:26:21 +0800

    add: english description for company 05

--- src/components/Company/Frame.vue
+++ src/components/Company/Frame.vue
@@ -111,7 +111,7 @@
                 <CompanyCardBox title="題目說明 #05" 
                                 imageSrc="Company/Logo/Logitech-logo.svg" 
                                 companyName="羅技" 
-                                description="羅技期望透過探索 AI 在各種使用場景中的潛在應用，不斷創新使用者體驗。結合現有的多種 AI 輔助技術與羅技產品，幫助使用者拓展能力、實現個人成就。"
+                                description="Beyond Boundaries: Enhance creativity, productivity and gaming experience with AI"
                                 detailsLink="Company/company_pdf/logitech-2.pdf" 
                                 imageClass="absolute top-1/2 left-1/2 w-[72.8%] h-auto transform -translate-x-1/2 -translate-y-1/2">
                 </CompanyCardBox>
