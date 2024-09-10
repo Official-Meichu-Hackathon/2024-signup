1fd79dd0bb0a8ea48fde9ae9d347289054f7a9ac
ChenCXxx <judy940825@gmail.com>
2024-09-10 11:47:28 +0800

    fix(Topic_desc): change company logo type (mobile version)

--- src/components/Company/MobileFrame.vue
+++ src/components/Company/MobileFrame.vue
@@ -6,7 +6,7 @@
         <div class="row-start-1 col-start-1 col-span-2">
             <div class="w-full flex h-[62vw] rounded-[3vw] border-[3px] bg-[#FFF] border-dark-gray">
                 <MobileCompanyCardBox title="題目說明 #01" 
-                                imageSrc="Company/Logo/logo-NXP.png" 
+                                imageSrc="Company/Logo/NXP-logo.png" 
                                 companyName="恩智浦半導體" 
                                 description="如何能夠以創新應用因應目前的氣候變遷，對企業來說是個非常重要的課題，希望參賽者能利用恩智浦與文曄科技提供的相關開發資源，實現智慧應用、符合永續需求。"
                                 detailsLink="Company/company_pdf/nxp.pdf" 
@@ -23,7 +23,7 @@
         <div class="row-start-2 col-start-2 col-span-2">
             <div class="w-full flex h-[62vw] rounded-[3vw] border-[3px] bg-[#FFF] border-dark-gray">
                 <MobileCompanyCardBox title="題目說明 #02" 
-                                    imageSrc="Company/Logo/logo-LINE.svg" 
+                                    imageSrc="Company/Logo/LINE-logo.svg" 
                                     companyName="LINE台灣" 
                                     description="以「透過生成式 AI 和多模態模型打造校園相關應用的 LINE Bot」為主題，創建一個能夠與校園生活緊密連結的 LINE Bot。"
                                     detailsLink="Company/company_pdf/line.pdf" 
@@ -35,7 +35,7 @@
         <div class="row-start-3 col-start-1 col-span-2">
             <div class="w-full flex h-[62vw] rounded-[3vw] border-[3px] bg-[#FFF] border-dark-gray">
                 <MobileCompanyCardBox title="題目說明 #03" 
-                                imageSrc="Company/Logo/Google-logo.svg" 
+                                imageSrc="Company/Logo/Google-logo.png" 
                                 companyName="Google" 
                                 description="使用 Gemini API 或 Gemma 模型在手機上做各種應用，透過 Gemini 發想能在生活中使用 AI 結合手機大幅提升效率或創新的可能。"
                                 detailsLink="Company/company_pdf/google.pdf" 
@@ -46,7 +46,7 @@
         <div class="row-start-4 col-start-1 col-span-2">
             <div class="w-full flex h-[62vw] rounded-[3vw] border-[3px] bg-[#FFF] border-dark-gray">
                 <MobileCompanyCardBox title="題目說明 #04" 
-                                imageSrc="Company/Logo/Tsmc-logo.svg" 
+                                imageSrc="Company/Logo/Tsmc-logo.png" 
                                 companyName="台積電" 
                                 description="思考即將進人職場的自己會希望得到什麼樣的員工數位認可 / 獎勵，並可選擇結合時下熱門的 Gen AI, Chatbot，實作出 Data-driven 且重視使用者體驗的解決方案。"
                                 detailsLink="Company/company_pdf/tsmc.pdf" 
@@ -79,7 +79,7 @@
         <div class="row-start-5 col-start-2 col-span-2">
             <div class="w-full flex h-[62vw] rounded-[3vw] border-[3px] bg-[#FFF] border-dark-gray">
                 <MobileCompanyCardBox title="題目說明 #05" 
-                                imageSrc="Company/Logo/Logitech-logo.svg" 
+                                imageSrc="Company/Logo/Logitech-logo.png" 
                                 companyName="羅技" 
                                 description="羅技期望透過探索 AI 在各種使用場景中的潛在應用，不斷創新使用者體驗。結合現有的多種 AI 輔助技術與羅技產品，幫助使用者拓展能力、實現個人成就。"
                                 detailsLink="Company/company_pdf/logitech.pdf" 
@@ -91,7 +91,7 @@
         <div class="row-start-6 col-start-1 col-span-2">
             <div class="w-full flex h-[62vw] rounded-[3vw] border-[3px] bg-[#FFF] border-dark-gray">
                 <MobileCompanyCardBox title="題目說明 #06" 
-                                imageSrc="Company/Logo/Chunghwa_Telecom-logo.svg" 
+                                imageSrc="Company/Logo/Chunghwa_Telecom-logo.png" 
                                 companyName="中華電信" 
                                 description="應用生成式 AI 或 AI 偵偽/檢測技術，針對各式議題，設計創新應用，以避免生成式 AI 合成語音及 AI 影像/影片生成對一般大眾的負面威脅。"
                                 detailsLink="Company/company_pdf/中華電信.pdf" 
